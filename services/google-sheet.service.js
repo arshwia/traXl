1cd44300f5de7c264ebb35900896783b5c568391
arshwia <arshia.hy.coding@gmail.com>
2025-12-03 19:23:47 +0330

    in 'services/google-sheet.service.js' 1. translate Dialogs , ba google sheet dialog ha ro trgomih kardim wa dobarh daryaftsh kardim

---
+++ services/google-sheet.service.js
@@ -0,0 +1,59 @@
+const { google, auth } = require('../config/google.config');
+const SHEET_ID = '1x7xYBLZq_CgEZXzYV4mYXdPqNrvsTHrhsvo0DHJZjMw';
+
+/**
+ * ترجمه و گرفتن دیالوگ ها با استفاده از گوگل شیت
+ */
+async function translateDialogs(dialogs) {
+    //احراز هویت و کانکت شدن به گوگل شیت
+    const client = await auth.getClient();
+    //ایجاد یک شیع برای برقراری ارتباط و خواندن و نوشتن
+    const sheet = google.sheets({ version: 'v4', auth: client });
+
+    //نوشتن دیالوگ های خام روی گوگل شیت
+    await sheet.spreadsheets.values.update({
+        spreadsheetId: SHEET_ID,
+        range: 'Sheet1!A1',
+        valueInputOption: 'RAW',
+        requestBody: {
+            values: dialogs.map((d) => [d.text]),
+        },
+    });
+
+    // اعمال دستور ترجمه با استفاده از گوگل ترنسلید
+    await sheet.spreadsheets.batchUpdate({
+        spreadsheetId: SHEET_ID,
+        requestBody: {
+            requests: [
+                {
+                    repeatCell: {
+                        range: {
+                            sheetId: 0,
+                            startColumnIndex: 1,
+                            endColumnIndex: 2,
+                            startRowIndex: 0,
+                        },
+                        cell: {
+                            userEnteredValue: {
+                                formulaValue: '=GOOGLETRANSLATE(A1,"en","fa")',
+                            },
+                        },
+                        fields: 'userEnteredValue',
+                    },
+                },
+            ],
+        },
+    });
+
+    //گرفتن دیالوگ های ترجمه شده
+    const result = await sheet.spreadsheets.values.get({
+        spreadsheetId: SHEET_ID,
+        range: 'Sheet1!B1:B',
+    });
+
+    // برگردونندن دیالوگ های ترجمه شده
+    return result.data.values?.map(([cell]) => cell || []);
+}
+
+// اکسپورت کردن فانکشنمون
+module.exports = { translateDialogs };
