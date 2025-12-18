200d0aebee0fda52fe5613a261f4c20fb546689a
arshwia <arshia.hy.coding@gmail.com>
2025-12-18 21:43:47 +0330

    in 'app.js' 1. delete console.clear()

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-console.clear();
-
 const { processSrtFile } = require('./controllers/srt.controller');
 
 // #TODO باید یک کاری کنم که یه کل سیستم دسترسی داشته و بتونه فقط با داشن ادرس فایل فایل رو ترجمه کنه و همون جا ترجمه رو بریزه
