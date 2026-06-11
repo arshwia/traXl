2a22c0b6f08b5e530fa44f431e615eb4fc433957
arshwia <arshia.hy.coding@gmail.com>
2026-06-11 11:26:34 +0330

    tonstim connct bshem

--- config/google.config.js
+++ config/google.config.js
@@ -2,7 +2,8 @@ const { google } = require('googleapis');
 
 //تنظیمات گوگل ای پی ای
 const auth = new google.auth.GoogleAuth({
-    keyFile: './services/google-sheet.service.js',
+    // keyFile: './services/google-sheet.service.js',
+    keyFile: './service-account.json',
     scopes: ['https://www.googleapis.com/auth/spreadsheets'],
 });
 
